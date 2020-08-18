a2e31e144b505af609c001c433a4d20b5f32597b
Ahmed Al-Majidi <af774857999@gmail.com>
2020-08-18 06:53:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function fetchBooks() {
   .then(function(response){
       return response.json();
   })
-  .then(function(json){
-    renderBooks(json);
+  .then(function(JSON){
+    renderBooks(JSON);
   })
 }
 
